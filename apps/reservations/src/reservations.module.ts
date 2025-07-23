4e029aadab77c89621a0a34fe74eb83e34c13cb7
Shivam Bhatia <shibhati1@WKMZTCD4CA82.global.publicisgroupe.net>
2025-07-23 13:24:22 +0530

    logger and validation

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,11 +1,22 @@
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, LoggerModule } from '@app/common';
 import { Module } from '@nestjs/common';
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './models/reservation.schema';
 import { ReservationsController } from './reservations.controller';
+import { ReservationsRepository } from './reservations.repository';
 import { ReservationsService } from './reservations.service';
 
 @Module({
-  imports: [DatabaseModule, ReservationsModule],
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: ReservationDocument.name, schema: ReservationSchema },
+    ]),
+    LoggerModule,
+  ],
   controllers: [ReservationsController],
-  providers: [ReservationsService],
+  providers: [ReservationsService, ReservationsRepository],
 })
 export class ReservationsModule {}
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,16 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '../config/config.module';
+
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGODB_URI'),
+        uri: configService.get('MONGODB_URI'),
       }),
       inject: [ConfigService],
+      imports: [ConfigModule],
     }),
   ],
 })
-export class DatabaseModule {}
+export class DatabaseModule {
+  static forFeature(models: ModelDefinition[]) {
+    return MongooseModule.forFeature(models);
+  }
+}
